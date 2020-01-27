2e016826847ab346604e3ab6c6b483be1a584af9
takuya hakiri <tyapi4@yahoo.co.jp>
2020-01-27 18:06:57 +0900

    add[computer_attackメソッドのテスト実装]

--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -15,4 +15,33 @@ RSpec.describe ShiritoriGame do
     subject(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
     it { expect(shiritori_game.start).to include("\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n") }
   end
+
+  describe '#computer_attack' do
+    subject(:shiritori_game_computer_attack) { shiritori_game.computer_attack }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    context 'ShiritoriGame#histories 最後が リンゴ のとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+
+      it 'ゴリラが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('ゴリラ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が パリ のとき' do
+      before { shiritori_game.histories << 'パリ' }
+
+      it 'リンゴが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('リンゴ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が リンゴ, ゴリラ, ラッパ, パリ 以外のとき' do
+      before { shiritori_game.histories << 'イヌ' }
+
+      it 'nil が返ってくること' do
+        expect(shiritori_game_computer_attack).to eq(nil)
+      end
+    end
+  end
 end
